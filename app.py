900b498bace09e529e154fcaa9d70b4002058554
Brenno Murakami <brenno.murakami@gmail.com>
2024-05-02 19:01:34 -0300

    gemini file database connection implemented

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, request, jsonify
 import google.generativeai as genai
+import sys
+import os
+
+# Adiciona o diretório raiz do projeto ao caminho de pesquisa de módulos
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Importa e inicializa os modelos
+from models.conta import db
+from models.conversa import db
+from models.historico_conversa import db
 
 app = Flask(__name__)
+
+# Configurações do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:28072003xennox@localhost/llm'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializa o banco de dados com o aplicativo Flask
+db.init_app(app)
+
+# Cria todas as tabelas do banco de dados
+with app.app_context():
+    db.create_all()
 
 genai.configure(api_key="AIzaSyBtEoIS8axNc2dqpVMNAy7Dap2XYoAwWUk")
 model = genai.GenerativeModel("gemini-pro")
