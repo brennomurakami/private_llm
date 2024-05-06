e6a514a75b7ccd624beb6016b67ce75e7ff9f00d
Brenno Murakami <brenno.murakami@gmail.com>
2024-05-06 03:37:05 -0300

    fixing database problems

--- models/conversa.py
+++ models/conversa.py
@@ -1,10 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from .conta import db
 
 class Conversa(db.Model):
     idconversa = db.Column(db.Integer, primary_key=True)
     nome_conversa = db.Column(db.String(45), nullable=False)
-    data = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    idconta = db.Column(db.Integer, db.ForeignKey('conta.idconta'), nullable=False)
+    idconta = db.Column(db.Integer, db.ForeignKey('conta.idconta'), nullable=True)
     conta = db.relationship('Conta', backref=db.backref('conversas', lazy=True))
--- models/historico_conversa.py
+++ models/historico_conversa.py
@@ -1,11 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from .conversa import db
 
 class HistoricoConversa(db.Model):
     idhistorico = db.Column(db.Integer, primary_key=True)
-    mensagem = db.Column(db.Text, nullable=False)
-    pergunta = db.Column(db.Boolean, nullable=False)
-    data_hora = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    resposta = db.Column(db.Text, nullable=False)
+    pergunta = db.Column(db.Text, nullable=False)
     idconversa = db.Column(db.Integer, db.ForeignKey('conversa.idconversa'), nullable=False)
     conversa = db.relationship('Conversa', backref=db.backref('historico', lazy=True))
